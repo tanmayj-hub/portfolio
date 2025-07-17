17b6cc84c0d280e8017eb27b10120857e9c0ef95
tanmayj-hub <tanmay.work96@outlook.com>
2025-07-16 21:08:28 -0400

    resume added

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -42,8 +42,8 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume.pdf")}
-                    download="Resume.pdf"
+                    href={require("./resume_tanmay_jhunjhunwala.pdf")}
+                    download="resume_tanmay_jhunjhunwala.pdf"
                     className="download-link-button"
                   >
                     <Button text="Download my resume" />
